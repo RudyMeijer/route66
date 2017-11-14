e8c5ff1b57e7b178ea6f1452741e1a2335fd5b49
MeijerR <MeijerR@NLHLP626>
2017-11-14 18:01:52 +0100

    Add Gmap

--- Route66/Form1.Designer.cs
+++ Route66/Form1.Designer.cs
@@ -29,12 +29,18 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(3, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -42,24 +48,73 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // webBrowser1
+            // splitContainer1
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(389, 310);
-            this.webBrowser1.TabIndex = 1;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.gmap);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(553, 338);
+            this.splitContainer1.SplitterDistance = 380;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(0, 0);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(380, 338);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 13D;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(90, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 310);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.webBrowser1);
+            this.ClientSize = new System.Drawing.Size(553, 338);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Route66";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,7 +122,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Route66/Form1.cs
+++ Route66/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GMap.NET.MapProviders;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,16 +16,47 @@ namespace Route66
         public Form1()
         {
             InitializeComponent();
+            Text += My.Version;
+            My.Log($"Start {this.Text}");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGmap();
+            InitializeComboboxWithMapProviders();
+        }
+
+        private void InitializeComboboxWithMapProviders()
+        {
+            //var x = GMap.NET.MapProviders.
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add(BingMapProvider.Instance);
+            comboBox1.Items.Add(GoogleMapProvider.Instance);
+            comboBox1.Items.Add(GoogleSatelliteMapProvider.Instance);
+            comboBox1.Items.Add(GoogleTerrainMapProvider.Instance);
+            comboBox1.Items.Add(OpenCycleMapProvider.Instance);
+            comboBox1.Items.Add(WikiMapiaMapProvider.Instance);
+            comboBox1.Items.Add(OpenStreetMapProvider.Instance);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void InitializeGmap()
+        {
+            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Zoom = 13;
+            gmap.SetPositionByKeywords("Holten");
+            gmap.ShowCenter = false;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            webBrowser1.Navigate(My.ExePath+"initialmap.html");
-            //webBrowser1.Url = new Uri("http://www.openstreetmap.org");
-            //NGEngine engine = NGEngine.createNew();
-            //var status = engine.init(dataDir: "C:\\ProgramData\\Aebi-Schmidt\\AutologicRouteCreator\\map\\data", mapDir: "C:\\ProgramData\\Aebi-Schmidt\\AutologicRouteCreator\\map");
-            //var result = engine.authorize(cid: "-----------CID-for-NaviGenie-team----------", aid: "PC_NaviGenie");
-            //var versie = engine.versionInfo();
+            InitializeComboboxWithMapProviders();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gmap.MapProvider = comboBox1.SelectedItem as GMapProvider;
         }
     }
 }
