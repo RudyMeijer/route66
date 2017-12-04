4a66632a5e613448dde5a88c496dfe6782b5d8c3
MeijerR <MeijerR@NLHLP626>
2017-12-04 23:03:59 +0100

    Add tabpages

--- Route66/Forms/FormEditChangeMarker.Designer.cs
+++ Route66/Forms/FormEditChangeMarker.Designer.cs
@@ -28,133 +28,44 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.numWidthLeft = new System.Windows.Forms.NumericUpDown();
+			this.btnRemove = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.numDosing = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.numWidthRight = new System.Windows.Forms.NumericUpDown();
 			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.numDosing = new System.Windows.Forms.NumericUpDown();
-			this.btnRemove = new System.Windows.Forms.Button();
-			this.btnSave = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.numWidthLeft)).BeginInit();
-			this.groupBox1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.numWidthRight)).BeginInit();
+			this.numWidthLeft = new System.Windows.Forms.NumericUpDown();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.tabPage5 = new System.Windows.Forms.TabPage();
+			this.tabPage6 = new System.Windows.Forms.TabPage();
+			this.tabPage7 = new System.Windows.Forms.TabPage();
+			this.tabPage8 = new System.Windows.Forms.TabPage();
+			this.tabPage9 = new System.Windows.Forms.TabPage();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numDosing)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numWidthRight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numWidthLeft)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// numWidthLeft
-			// 
-			this.numWidthLeft.DecimalPlaces = 1;
-			this.numWidthLeft.Increment = new decimal(new int[] {
-			5,
-			0,
-			0,
-			65536});
-			this.numWidthLeft.Location = new System.Drawing.Point(37, 14);
-			this.numWidthLeft.Maximum = new decimal(new int[] {
-			95,
-			0,
-			0,
-			65536});
-			this.numWidthLeft.Name = "numWidthLeft";
-			this.numWidthLeft.Size = new System.Drawing.Size(36, 20);
-			this.numWidthLeft.TabIndex = 2;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.numWidthRight);
-			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Controls.Add(this.numWidthLeft);
-			this.groupBox1.Location = new System.Drawing.Point(12, 65);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(172, 47);
-			this.groupBox1.TabIndex = 3;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Width";
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(85, 16);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(32, 13);
-			this.label1.TabIndex = 5;
-			this.label1.Text = "Right";
-			// 
-			// numWidthRight
-			// 
-			this.numWidthRight.DecimalPlaces = 1;
-			this.numWidthRight.Increment = new decimal(new int[] {
-			5,
-			0,
-			0,
-			65536});
-			this.numWidthRight.Location = new System.Drawing.Point(123, 14);
-			this.numWidthRight.Maximum = new decimal(new int[] {
-			95,
-			0,
-			0,
-			65536});
-			this.numWidthRight.Name = "numWidthRight";
-			this.numWidthRight.Size = new System.Drawing.Size(36, 20);
-			this.numWidthRight.TabIndex = 4;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(6, 16);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(25, 13);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Left";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(51, 22);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(38, 13);
-			this.label3.TabIndex = 4;
-			this.label3.Text = " gr/m2";
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Controls.Add(this.numDosing);
-			this.groupBox2.Controls.Add(this.label3);
-			this.groupBox2.Location = new System.Drawing.Point(12, 12);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(95, 47);
-			this.groupBox2.TabIndex = 5;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Dosage";
-			// 
-			// numDosing
-			// 
-			this.numDosing.DecimalPlaces = 1;
-			this.numDosing.Location = new System.Drawing.Point(9, 20);
-			this.numDosing.Maximum = new decimal(new int[] {
-			90,
-			0,
-			0,
-			0});
-			this.numDosing.Name = "numDosing";
-			this.numDosing.Size = new System.Drawing.Size(44, 20);
-			this.numDosing.TabIndex = 6;
-			this.numDosing.Value = new decimal(new int[] {
-			20,
-			0,
-			0,
-			0});
 			// 
 			// btnRemove
 			// 
-			this.btnRemove.Location = new System.Drawing.Point(126, 127);
+			this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnRemove.Location = new System.Drawing.Point(487, 333);
+			this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnRemove.Name = "btnRemove";
-			this.btnRemove.Size = new System.Drawing.Size(75, 23);
+			this.btnRemove.Size = new System.Drawing.Size(100, 28);
 			this.btnRemove.TabIndex = 6;
 			this.btnRemove.Text = "Remove";
 			this.btnRemove.UseVisualStyleBackColor = true;
@@ -162,51 +73,296 @@
 			// 
 			// btnSave
 			// 
-			this.btnSave.Location = new System.Drawing.Point(207, 127);
+			this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSave.Location = new System.Drawing.Point(595, 333);
+			this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnSave.Name = "btnSave";
-			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.Size = new System.Drawing.Size(100, 28);
 			this.btnSave.TabIndex = 7;
 			this.btnSave.Text = "Save";
 			this.btnSave.UseVisualStyleBackColor = true;
 			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
 			// 
+			// tabControl1
+			// 
+			this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Controls.Add(this.tabPage5);
+			this.tabControl1.Controls.Add(this.tabPage6);
+			this.tabControl1.Controls.Add(this.tabPage7);
+			this.tabControl1.Controls.Add(this.tabPage8);
+			this.tabControl1.Controls.Add(this.tabPage9);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(704, 315);
+			this.tabControl1.TabIndex = 8;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.groupBox2);
+			this.tabPage1.Controls.Add(this.groupBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 4);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(696, 286);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.button1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 4);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(523, 286);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.numDosing);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Location = new System.Drawing.Point(7, 7);
+			this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+			this.groupBox2.Size = new System.Drawing.Size(127, 58);
+			this.groupBox2.TabIndex = 7;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Dosage";
+			// 
+			// numDosing
+			// 
+			this.numDosing.DecimalPlaces = 1;
+			this.numDosing.Location = new System.Drawing.Point(12, 25);
+			this.numDosing.Margin = new System.Windows.Forms.Padding(4);
+			this.numDosing.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+			this.numDosing.Name = "numDosing";
+			this.numDosing.Size = new System.Drawing.Size(59, 22);
+			this.numDosing.TabIndex = 6;
+			this.numDosing.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(68, 27);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(48, 17);
+			this.label3.TabIndex = 4;
+			this.label3.Text = " gr/m2";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.numWidthRight);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.numWidthLeft);
+			this.groupBox1.Location = new System.Drawing.Point(7, 72);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+			this.groupBox1.Size = new System.Drawing.Size(229, 58);
+			this.groupBox1.TabIndex = 6;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Width";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(113, 20);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 17);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Right";
+			// 
+			// numWidthRight
+			// 
+			this.numWidthRight.DecimalPlaces = 1;
+			this.numWidthRight.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+			this.numWidthRight.Location = new System.Drawing.Point(164, 17);
+			this.numWidthRight.Margin = new System.Windows.Forms.Padding(4);
+			this.numWidthRight.Maximum = new decimal(new int[] {
+            95,
+            0,
+            0,
+            65536});
+			this.numWidthRight.Name = "numWidthRight";
+			this.numWidthRight.Size = new System.Drawing.Size(48, 22);
+			this.numWidthRight.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 20);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 17);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Left";
+			// 
+			// numWidthLeft
+			// 
+			this.numWidthLeft.DecimalPlaces = 1;
+			this.numWidthLeft.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+			this.numWidthLeft.Location = new System.Drawing.Point(49, 17);
+			this.numWidthLeft.Margin = new System.Windows.Forms.Padding(4);
+			this.numWidthLeft.Maximum = new decimal(new int[] {
+            95,
+            0,
+            0,
+            65536});
+			this.numWidthLeft.Name = "numWidthLeft";
+			this.numWidthLeft.Size = new System.Drawing.Size(48, 22);
+			this.numWidthLeft.TabIndex = 2;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Location = new System.Drawing.Point(4, 4);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(523, 286);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "tabPage3";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Location = new System.Drawing.Point(4, 4);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Size = new System.Drawing.Size(523, 286);
+			this.tabPage4.TabIndex = 3;
+			this.tabPage4.Text = "tabPage4";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// tabPage5
+			// 
+			this.tabPage5.Location = new System.Drawing.Point(4, 4);
+			this.tabPage5.Name = "tabPage5";
+			this.tabPage5.Size = new System.Drawing.Size(523, 286);
+			this.tabPage5.TabIndex = 4;
+			this.tabPage5.Text = "tabPage5";
+			this.tabPage5.UseVisualStyleBackColor = true;
+			// 
+			// tabPage6
+			// 
+			this.tabPage6.Location = new System.Drawing.Point(4, 4);
+			this.tabPage6.Name = "tabPage6";
+			this.tabPage6.Size = new System.Drawing.Size(523, 286);
+			this.tabPage6.TabIndex = 5;
+			this.tabPage6.Text = "tabPage6";
+			this.tabPage6.UseVisualStyleBackColor = true;
+			// 
+			// tabPage7
+			// 
+			this.tabPage7.Location = new System.Drawing.Point(4, 4);
+			this.tabPage7.Name = "tabPage7";
+			this.tabPage7.Size = new System.Drawing.Size(523, 286);
+			this.tabPage7.TabIndex = 6;
+			this.tabPage7.Text = "tabPage7";
+			this.tabPage7.UseVisualStyleBackColor = true;
+			// 
+			// tabPage8
+			// 
+			this.tabPage8.Location = new System.Drawing.Point(4, 4);
+			this.tabPage8.Name = "tabPage8";
+			this.tabPage8.Size = new System.Drawing.Size(523, 286);
+			this.tabPage8.TabIndex = 7;
+			this.tabPage8.Text = "tabPage8";
+			this.tabPage8.UseVisualStyleBackColor = true;
+			// 
+			// tabPage9
+			// 
+			this.tabPage9.Location = new System.Drawing.Point(4, 4);
+			this.tabPage9.Name = "tabPage9";
+			this.tabPage9.Size = new System.Drawing.Size(523, 286);
+			this.tabPage9.TabIndex = 8;
+			this.tabPage9.Text = "tabPage9";
+			this.tabPage9.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(212, 74);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
 			// FormEditChangeMarker
 			// 
 			this.AcceptButton = this.btnSave;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(294, 162);
+			this.ClientSize = new System.Drawing.Size(704, 374);
 			this.Controls.Add(this.btnSave);
 			this.Controls.Add(this.btnRemove);
-			this.Controls.Add(this.groupBox2);
-			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.tabControl1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "FormEditChangeMarker";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "Change marker";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormEditChangeMarker_FormClosing);
-			((System.ComponentModel.ISupportInitialize)(this.numWidthLeft)).EndInit();
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.numWidthRight)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
 			this.groupBox2.ResumeLayout(false);
 			this.groupBox2.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numDosing)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numWidthRight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numWidthLeft)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-		private System.Windows.Forms.NumericUpDown numWidthLeft;
+		private System.Windows.Forms.Button btnRemove;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.NumericUpDown numDosing;
+		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.NumericUpDown numWidthRight;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.GroupBox groupBox2;
-		private System.Windows.Forms.Button btnRemove;
-		private System.Windows.Forms.Button btnSave;
-		private System.Windows.Forms.NumericUpDown numDosing;
+		private System.Windows.Forms.NumericUpDown numWidthLeft;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.TabPage tabPage5;
+		private System.Windows.Forms.TabPage tabPage6;
+		private System.Windows.Forms.TabPage tabPage7;
+		private System.Windows.Forms.TabPage tabPage8;
+		private System.Windows.Forms.TabPage tabPage9;
 	}
 }
