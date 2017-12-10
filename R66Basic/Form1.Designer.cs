142ed51dd825b5c89fe532f0fe038e90daa7d366
MeijerR <MeijerR@NLHLP626>
2017-12-10 18:38:36 +0100

    Add R66basic

---
+++ R66Basic/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace R66Basic
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.gmap = new GMap.NET.WindowsForms.GMapControl();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.statusStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(867, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 500);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(867, 25);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+			this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.gmap);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.btnClear);
+			this.splitContainer1.Size = new System.Drawing.Size(867, 476);
+			this.splitContainer1.SplitterDistance = 729;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// gmap
+			// 
+			this.gmap.Bearing = 0F;
+			this.gmap.CanDragMap = true;
+			this.gmap.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+			this.gmap.GrayScaleMode = false;
+			this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+			this.gmap.LevelsKeepInMemmory = 5;
+			this.gmap.Location = new System.Drawing.Point(0, 0);
+			this.gmap.MarkersEnabled = true;
+			this.gmap.MaxZoom = 20;
+			this.gmap.MinZoom = 2;
+			this.gmap.MouseWheelZoomEnabled = true;
+			this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+			this.gmap.Name = "gmap";
+			this.gmap.NegativeMode = false;
+			this.gmap.PolygonsEnabled = true;
+			this.gmap.RetryLoadTile = 0;
+			this.gmap.RoutesEnabled = true;
+			this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+			this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+			this.gmap.ShowTileGridLines = false;
+			this.gmap.Size = new System.Drawing.Size(729, 476);
+			this.gmap.TabIndex = 0;
+			this.gmap.Zoom = 0D;
+			this.gmap.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gmap_OnMarkerClick);
+			this.gmap.OnMarkerEnter += new GMap.NET.WindowsForms.MarkerEnter(this.gmap_OnMarkerEnter);
+			this.gmap.OnMarkerLeave += new GMap.NET.WindowsForms.MarkerLeave(this.gmap_OnMarkerLeave);
+			this.gmap.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gmap_MouseDoubleClick);
+			this.gmap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.gmap_MouseDown);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(26, 116);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Vletgaarsmaten";
+			this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(22, 32);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 0;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(867, 525);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private GMap.NET.WindowsForms.GMapControl gmap;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+	}
+}
+
---
+++ R66Basic/Form1.cs
@@ -0,0 +1,113 @@
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.ObjectModel;
+
+namespace R66Basic
+{
+	public partial class Form1 : Form
+	{
+		private List<GMapMarker> PointCloud;
+		private readonly GMapOverlay Red;
+		private readonly GMapRoute RedRoute;
+
+		public bool IsOnMarker { get { return PointCloud.Count > 0; } }
+
+		public Form1()
+		{
+			InitializeComponent();
+			PointCloud = new List<GMapMarker>();
+			My.SetStatus(toolStripStatusLabel1);
+
+			gmap.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+			GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+			gmap.Overlays.Add(new GMapOverlay("Red"));
+			gmap.Overlays.Add(new GMapOverlay("Green"));
+			gmap.Overlays.Add(new GMapOverlay("Blue"));
+
+			Red = gmap.Overlays[0];
+			RedRoute = new GMapRoute("RedRoute");
+			RedRoute.Stroke = new Pen(Color.Red,2);
+			Red.Routes.Add(RedRoute);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			foreach (var overlay in gmap.Overlays)
+			{
+				overlay.Markers.Clear();
+				foreach (var route in overlay.Routes)
+				{
+					route.Clear();
+					gmap.UpdateRouteLocalPosition(route);
+				}
+			}
+			PointCloud.Clear();
+		}
+
+		private void gmap_MouseDown(object sender, MouseEventArgs e)
+		{
+			Console.WriteLine($"{DateTime.Now} gmap_MouseDown {e.Button} {e.Clicks} ({e.X},{e.Y})");
+			if (e.Button == MouseButtons.Left && !IsOnMarker) AddMarker(e);
+		}
+
+		private void gmap_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Console.WriteLine($"{DateTime.Now} gmap_MouseDoubleClick {e.Button} {e.Clicks} ({e.X},{e.Y})");
+		}
+
+		private void gmap_OnMarkerClick(GMap.NET.WindowsForms.GMapMarker item, MouseEventArgs e)
+		{
+			Console.WriteLine($"{DateTime.Now} gmap_OnMarkerClick {item.ToolTipText} {e.Button} {e.Clicks}");
+			if (e.Button == MouseButtons.Right) RemoveMarker(item);
+		}
+
+		private void textBox1_Validated(object sender, EventArgs e)
+		{
+			gmap.Zoom = 15;
+			gmap.SetPositionByKeywords(textBox1.Text);
+		}
+
+		private void gmap_OnMarkerEnter(GMapMarker item)
+		{
+			item.ToolTipText = $"{item.Overlay.Markers.IndexOf(item)}";
+			Console.WriteLine($"{DateTime.Now} gmap_OnMarkerEnter {item.ToolTipText} pointcloud {PointCloud.Count+1}");
+			PointCloud.Add(item);
+
+		}
+
+		private void gmap_OnMarkerLeave(GMapMarker item)
+		{
+			//Console.WriteLine($"{DateTime.Now} gmap_OnMarkerLeave {item.ToolTipText}");
+			PointCloud.Remove(item);
+		}
+
+		private void AddMarker(MouseEventArgs e)
+		{
+			Console.WriteLine($"{DateTime.Now} AddMarker ({e.X},{e.Y})");
+			PointLatLng point = gmap.FromLocalToLatLng(e.X, e.Y);
+			var marker = new GMarkerGoogle(point, GMarkerGoogleType.red_small);
+			Red.Markers.Add(marker);
+			RedRoute.Points.Add(point);
+			marker.ToolTipText = $"{marker.Overlay.Markers.Count-1}";
+			gmap.UpdateRouteLocalPosition(RedRoute);
+		}
+		private void RemoveMarker(GMapMarker item)
+		{
+			Red.Markers.Remove(item);
+			RedRoute.Points.Remove(item.Position);
+			gmap.UpdateRouteLocalPosition(RedRoute);
+			PointCloud.Remove(item);
+		}
+	}
+}
